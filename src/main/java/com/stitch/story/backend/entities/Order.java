781b860b7f7f83d8b83009e6d7addf9d1ae03328
Raman <kayastharaman967@gmail.com>
2026-09-05 18:01:08 +0545

    some final changes

--- src/main/java/com/stitch/story/backend/entities/Order.java
+++ src/main/java/com/stitch/story/backend/entities/Order.java
@@ -17,7 +17,7 @@ import java.util.List;
 @Builder
 @Entity
 @Table(name = "orders")
-public class ~Order {
+public class Order {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
